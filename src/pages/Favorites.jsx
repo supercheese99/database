212dc166043adad1d2d97abae930dee632471cea
oliwia <wegneroliwia@outlook.com>
2024-06-02 18:57:00 -0700

    the favorite function is fully functional

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { useFavorites } from '../components/FavoritesContext';
 
 const Favorites = () => {
-  const { favorites } = useFavorites();
+  const { addToFavorites, removeFromFavorites, favorites } = useFavorites();
 
   useEffect(() => {
     document.title = 'Favorites';
@@ -14,6 +14,17 @@ const Favorites = () => {
       return `http://image.tmdb.org/t/p/${size}${path}`;
   }
 
+  // making sure one can remove a movie from their favorites list from the
+  // list level, instead of going to the single movie page to do that
+  const handleToggleFavorite = (movie) => {
+    const isFavorite = favorites.some(fav => fav.id === movie.id);
+    if (isFavorite) {
+      removeFromFavorites(movie.id);
+    } else {
+      addToFavorites(movie);
+    }
+  };
+
   return (
     <div className="favorites-wrapper">
       <h1>Favorites</h1>
@@ -22,17 +33,23 @@ const Favorites = () => {
           <p>No favorite movies yet. Add some from the <Link to='/'>Movie List!</Link></p>
         </div>
       ) : (
-        <ul>
-          {favorites.map((movie) => (
-            // const image = buildImage(poster_path, "w500");
-        <Link to={`/movie/${movie.id}`}>
-          {/* <img src={image} className="movie-list-img" /> */}
-          <li key={movie.id}>{movie.title}</li>
-        </Link>
-
-            
-          ))}
-        </ul>
+        <div className="favorites-grid">
+          {favorites.map((movie) => {
+            const image = buildImage(movie.poster_path, "w500");
+            const isFavorite = movie && favorites.some(fav => fav.id === movie.id);
+            return (
+              <div key={movie.id} className="favorite-item">
+                <Link to={`/movie/${movie.id}`}>
+                  <img src={image} alt={movie.title} className="movie-list-img" />
+                  <div className="favorite-title">{movie.title}</div>
+                </Link>
+                <button onClick={() => handleToggleFavorite(movie)} className="fave-button">
+                    {isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
+                </button>
+              </div>
+            );
+          })}
+        </div>
       )}
     </div>
   );
